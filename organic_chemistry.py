ff3b3bdb7749a7012ed417471b9bccfa1ec7ede3
Qrandom5717 <110209827+random5717@users.noreply.github.com>
2025-01-21 20:35:29 +0800

    完成: 添加chemistry_feature.json文件(待修改) 初步完成对feature属性的实现 待完成: 对chemistry_feature.json修改,设立命名规则使其能根据代码实现哈希, 添加'别名',完善哈希表

--- organic_chemistry.py
+++ organic_chemistry.py
@@ -1,5 +1,35 @@
+import json
 CHEMISTRY_BOND_DICT = {'C': 4, 'O': 2, 'N': 3}
-FEATURE_TABLE = {'C': 0b1000000, 'O': 0b0100000}
+HASH_FEATURE_TABLE = {}
+
+with open("chemistry_feature.json", 'r', encoding='utf-8') as f:
+    raw_feature_table = json.loads(f.read())
+del f
+
+MAX_FEATURE_NUM = 0
+tmp_index = 1
+for i in raw_feature_table:
+    if i == "comment":
+        continue
+    HASH_FEATURE_TABLE[i] = {}
+    for j in raw_feature_table[i]:
+        MAX_FEATURE_NUM += 1
+        HASH_FEATURE_TABLE[i][j] = tmp_index
+        tmp_index += 1
+
+del tmp_index
+del raw_feature_table
+print(MAX_FEATURE_NUM, HASH_FEATURE_TABLE)
+
+
+def add_feature(element_feature, feature_class, feature):
+    element_feature |= (1 << HASH_FEATURE_TABLE[feature_class][feature])
+    return element_feature
+
+
+def del_feature(element_feature, feature_class, feature):
+    element_feature &= ~(1 << HASH_FEATURE_TABLE[feature_class][feature])
+    return element_feature
 
 
 def connect(target_element_list, is_cyclization=False):
@@ -17,6 +47,8 @@ class Element:
             raise TypeError
         self.name = name
         self.bond_list = [None for _ in range(CHEMISTRY_BOND_DICT[name])]
+        self.feature = 1 << MAX_FEATURE_NUM
+        self.feature = add_feature(self.feature, "self", self.name.lower())
         # self.feature = 0b00000
         # self.feature += FEATURE_TABLE[name]
 
@@ -32,6 +64,10 @@ class Element:
             for _ in range(connect_num):
                 self.bond_list[left_index] = target_element
                 left_index += 1
+
+            # 待实现: 实际调用
+            self.feature = add_feature()
+
             if is_first:
                 target_element.add_bond(self, connect_num, False)
         # self.feature += FEATURE_TABLE[str(connect_num) + target_element]
@@ -57,6 +93,7 @@ c4 = Element("c")
 c5 = Element("c")
 c6 = Element("c")
 c7 = Element("c")
+# c1-c7 o1 构成苯醛
 o1 = Element("o")
 o2 = Element("o")
 o3 = Element("o")
@@ -66,5 +103,15 @@ connect([c2, c3, c4, c5, c6, c7], True)
 
 c2.add_pi_pond([c2, c3, c4, c5, c6, c7])
 
+for i in c1.bond_list:
+    if type(i) is Element:
+        print(i.name, i.feature)
+print()
+for i in c2.bond_list:
+    if type(i) is Element:
+        print(i.name, i.feature)
+    elif type(i) is list:
+        for j in i:
+            print(j.name, j.feature)
 print(c1.bond_list, c1)
 print(c2.bond_list, c2.name)
