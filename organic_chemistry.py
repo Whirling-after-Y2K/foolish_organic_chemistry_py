4f68ce4c95d61a2361d926a1c7636c4b88c463f2
Qrandom5717 <110209827+random5717@users.noreply.github.com>
2025-01-22 13:06:24 +0800

    完成命名规则2.0(待修改) 优化(大概)

--- organic_chemistry.py
+++ organic_chemistry.py
@@ -1,4 +1,5 @@
 import json
+
 CHEMISTRY_BOND_DICT = {'C': 4, 'O': 2, 'N': 3}
 HASH_FEATURE_TABLE = {}
 
@@ -6,54 +7,41 @@ with open("chemistry_feature.json", 'r', encoding='utf-8') as f:
     raw_feature_table = json.loads(f.read())
 del f
 
-MAX_FEATURE_NUM = 0
+MAX_FEATURE_NUM = 5  # 前五位用于储存self_name
 tmp_index = 1
+
 for i in raw_feature_table:
     if i == "comment":
         continue
-    HASH_FEATURE_TABLE[i] = {}
-    for j in raw_feature_table[i]:
-        MAX_FEATURE_NUM += 1
-        HASH_FEATURE_TABLE[i][j] = tmp_index
-        tmp_index += 1
+    elif i == "self_name":
+        HASH_FEATURE_TABLE[i] = {}
+        tmp_index2 = 0
+        for j in raw_feature_table[i]:
+            HASH_FEATURE_TABLE[i][j[0]] = tmp_index2
+            HASH_FEATURE_TABLE[i][tmp_index2] = j[0]
+            tmp_index2 += 1
+        del tmp_index2
+    else:
+        HASH_FEATURE_TABLE[i] = {}
+        for j in raw_feature_table[i]:
+            MAX_FEATURE_NUM += 1
+            HASH_FEATURE_TABLE[i][j[0]] = tmp_index
+            tmp_index += 1
 
 del tmp_index
 del raw_feature_table
 print(MAX_FEATURE_NUM, HASH_FEATURE_TABLE)
 
 
-def add_feature(element_feature, feature_class, feature):
-    element_feature |= (1 << HASH_FEATURE_TABLE[feature_class][feature])
-    return element_feature
-
-
-def del_feature(element_feature, feature_class, feature):
-    element_feature &= ~(1 << HASH_FEATURE_TABLE[feature_class][feature])
-    return element_feature
-
-
-def connect(target_element_list, is_cyclization=False):
-    for i in range(len(target_element_list) - 1):
-        target_element_list[i].add_bond(target_element_list[i + 1])
-    if is_cyclization:
-        target_element_list[0].add_bond(target_element_list[-1])
-
-
 class Element:
-    def __init__(self, name):
-        if type(name) is str:
-            name = name.upper()
-        else:
-            raise TypeError
+    def __init__(self, name: str):
+        name = name.lower()
         self.name = name
         self.bond_list = [None for _ in range(CHEMISTRY_BOND_DICT[name])]
         self.feature = 1 << MAX_FEATURE_NUM
-        self.feature = add_feature(self.feature, "self", self.name.lower())
+        self.feature = add_feature(self.feature, "self_name", self.name)
         # self.feature = 0b00000
         # self.feature += FEATURE_TABLE[name]
-
-    # def __eq__(self, other):
-    #     return self.feature == other.feature
 
     def add_bond(self, target_element, connect_num=1, is_first=True):
         try:
@@ -66,16 +54,19 @@ class Element:
                 left_index += 1
 
             # 待实现: 实际调用
-            self.feature = add_feature()
+            # self.feature = update_feature(self.feature, "direct_connection", target_element, connect_num)
 
             if is_first:
                 target_element.add_bond(self, connect_num, False)
         # self.feature += FEATURE_TABLE[str(connect_num) + target_element]
 
-    def add_pi_pond(self, target_element_list, is_first=True):  # 注意target_element_list包含自身
-        tmp_list = target_element_list.copy()
+    # def __eq__(self, other):
+    #     return self.feature == other.feature
+
+    def add_pi_pond(self, target_element_list: list, is_first=True):  # 注意target_element_list包含自身
+        tmp_list: list = target_element_list.copy()
         try:
-            left_index = self.bond_list.index(None)
+            left_index: int = self.bond_list.index(None)
         except ValueError:
             raise IndexError("list index out of range")
         else:
@@ -84,6 +75,40 @@ class Element:
                 target_element_list.remove(self)
                 for target_element in target_element_list:
                     target_element.add_pi_pond(tmp_list, False)
+
+
+def inquire_feature(element_feature: int, feature_class: str, feature: str):
+    if feature_class == "self_name":
+        return element_feature & (1 << 5)
+    return element_feature & (1 << HASH_FEATURE_TABLE[feature_class][feature])
+
+
+def add_feature(element_feature: int, feature_class: str, feature: str):
+    if feature_class == "self_name":
+        element_feature |= HASH_FEATURE_TABLE[feature_class][feature]
+    element_feature |= (1 << HASH_FEATURE_TABLE[feature_class][feature])
+    return element_feature
+
+
+def del_feature(element_feature: int, feature_class: str, feature: str):
+    if feature_class == "self_name":
+        raise TypeError
+    element_feature &= ~(1 << HASH_FEATURE_TABLE[feature_class][feature])
+    return element_feature
+
+
+def update_feature(element_feature: int, feature_class: str, target_element: Element, connect_num: int):
+    feature_name = HASH_FEATURE_TABLE["self_name"][element_feature & (1 << 5)] + "-" + str(
+        connect_num) + target_element.name
+
+    return
+
+
+def connect(target_element_list, is_cyclization=False):
+    for i in range(len(target_element_list) - 1):
+        target_element_list[i].add_bond(target_element_list[i + 1])
+    if is_cyclization:
+        target_element_list[0].add_bond(target_element_list[-1])
 
 
 c1 = Element("C")
