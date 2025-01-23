d2408b4ecc2f6911dc5afadfbc2c07548426d26d
Qrandom5717 <110209827+random5717@users.noreply.github.com>
2025-01-23 10:49:18 +0800

    完成命名规则最终版

--- organic_chemistry.py
+++ organic_chemistry.py
@@ -2,76 +2,77 @@ import json
 
 CHEMISTRY_BOND_DICT = {'c': 4, 'o': 2, 'n': 3}
 HASH_FEATURE_TABLE = {}
-SAVE_NAME_NUM = 7
-MAX_FEATURE_NUM = SAVE_NAME_NUM  # 后7位用于储存self_name 元素名
-MAX_ID_NUM = 11  # 第一位弃用,二到十二位用于储存element_id 最多容纳2^11-2个原子
-element_id = -1
+SAVE_NAME_NUM = 4
+MAX_FEATURE_NUM = SAVE_NAME_NUM  # 后4位用于储存self_name 元素名
+# MAX_ID_NUM = 11  # 第一位弃用,二到十二位用于储存element_id 最多容纳2^11-2个原子
+# element_id = -1
 
 with open("chemistry_feature.json", 'r', encoding='utf-8') as f:
     raw_feature_table = json.loads(f.read())
 del f
 
-tmp_index = 1
-
 for i in raw_feature_table:
     if i == "comment":
         continue
     elif i == "self_name":
-        HASH_FEATURE_TABLE[i] = {}
+
         tmp_index2 = 0
         for j in raw_feature_table[i]:
-            HASH_FEATURE_TABLE[i][j[0]] = tmp_index2
-            HASH_FEATURE_TABLE[i][tmp_index2] = j[0]
+            HASH_FEATURE_TABLE[j[0]] = tmp_index2  # tmp_index2用于为每一种名称编号 表示储存在后5位中该元素为tmp_index2
+            HASH_FEATURE_TABLE[tmp_index2] = j[0]
             tmp_index2 += 1
         del tmp_index2
     else:
-        HASH_FEATURE_TABLE[i] = {}
+        tmp_index = SAVE_NAME_NUM
+        tmp_feature_num = SAVE_NAME_NUM
         for j in raw_feature_table[i]:
-            MAX_FEATURE_NUM += 1  # 每多一种特征就需要多一位进行储存
-            HASH_FEATURE_TABLE[i][j[0]] = tmp_index
+            tmp_feature_num += 1  # 每多一种特征就需要多一位进行储存,但不同元素的特征不可能共存 tmp_feature_num用于比较至少需要多少位进行储存
+            HASH_FEATURE_TABLE[j[0]] = tmp_index  # tmp_index用于为每一种特征编号 表示其储存在第tmp_index+1位
             tmp_index += 1
+
+        MAX_FEATURE_NUM = max(tmp_feature_num - 1, MAX_FEATURE_NUM)
 
 del tmp_index
 del raw_feature_table
 print(MAX_FEATURE_NUM, HASH_FEATURE_TABLE)
 
 
-class Element:
+class Atom:
     def __init__(self, name: str):
-        global element_id
-        element_id += 1
-        if element_id > ((1 << MAX_ID_NUM) - 2):
-            raise OverflowError
+        # global element_id
+        # element_id += 1
+        # if element_id > ((1 << MAX_ID_NUM) - 2):
+        #     raise OverflowError
         name = name.lower()
         self.name = name
         self.bond_list = [None for _ in range(CHEMISTRY_BOND_DICT[name])]
         self.feature = 1 << MAX_FEATURE_NUM
-        self.feature += (element_id << MAX_FEATURE_NUM)
-        self.feature |= HASH_FEATURE_TABLE["self_name"][self.name]
+        # self.feature += (element_id << MAX_FEATURE_NUM)
+        self.feature |= HASH_FEATURE_TABLE[self.name]
         # self.feature = 0b00000
         # self.feature += FEATURE_TABLE[name]
 
-    def add_bond(self, target_element, connect_num=1, is_first=True):
+    def add_bond(self, target_atom, connect_num=1, is_first=True):
         try:
             left_index = self.bond_list.index(None)
         except ValueError:
             raise IndexError("list index out of range")
         else:
             for _ in range(connect_num):
-                self.bond_list[left_index] = target_element
+                self.bond_list[left_index] = target_atom
                 left_index += 1
 
-            update_feature(self, target_element, connect_num)
+            update_feature(self, target_atom, connect_num)
 
             if is_first:
-                target_element.add_bond(self, connect_num, False)
+                target_atom.add_bond(self, connect_num, False)
         # self.feature += FEATURE_TABLE[str(connect_num) + target_element]
 
     # def __eq__(self, other):
     #     return self.feature == other.feature
 
-    def add_pi_pond(self, target_element_list: list, is_first=True):  # 注意target_element_list包含自身
-        tmp_list: list = target_element_list.copy()
+    def add_pi_pond(self, target_atom_list: list, is_first=True):  # 注意target_element_list包含自身
+        tmp_list: list = target_atom_list.copy()
         try:
             left_index: int = self.bond_list.index(None)
         except ValueError:
@@ -79,87 +80,95 @@ class Element:
         else:
             self.bond_list[left_index] = tmp_list
             if is_first:
-                target_element_list.remove(self)
-                for target_element in target_element_list:
-                    target_element.add_pi_pond(tmp_list, False)
+                target_atom_list.remove(self)
+                for target_atom in target_atom_list:
+                    target_atom.add_pi_pond(tmp_list, False)
 
 
-def inquire_id(element_feature: int):
-    return (element_feature & (((1 << (MAX_FEATURE_NUM + MAX_ID_NUM)) - 1) & (~((1 << MAX_FEATURE_NUM) - 1))))>>MAX_FEATURE_NUM
+class Molecule:
+    def __init__(self):
+        self.composition = []
+
+    def add_atom(self, atom_name, num=1):
+        for _ in range(num):
+            self.composition.append(Atom(atom_name))
+
+
+'''
+def inquire_id(atom_feature: int):
+    return (atom_feature & (((1 << (MAX_FEATURE_NUM + MAX_ID_NUM)) - 1) & (~((1 << MAX_FEATURE_NUM) - 1))))>>MAX_FEATURE_NUM
 # 用于实现对id的查询,至于为什么不用字符串切片,大概是因为位运算它快吧
+'''
+
 
 # def update_id()
 
-def inquire_feature(element_feature: int, feature_class: str, feature: str):
-    if feature_class == "self_name":
-        return element_feature & (1 << SAVE_NAME_NUM)
-    return element_feature & (1 << HASH_FEATURE_TABLE[feature_class][feature])
+def inquire_feature(atom_feature: int, feature: str):
+    return atom_feature & (1 << HASH_FEATURE_TABLE[feature])
 
 
-def add_feature(element: Element, feature_class: str, feature: str):
-    element.feature |= (1 << HASH_FEATURE_TABLE[feature_class][feature])
+def add_feature(atom: Atom, feature: str):
+    atom.feature |= (1 << HASH_FEATURE_TABLE[feature])
 
 
-def del_feature(element: Element, feature_class: str, feature: str):
-    if feature_class == "self_name":
-        raise TypeError
-    else:
-        element.feature &= ~(1 << HASH_FEATURE_TABLE[feature_class][feature])
+def del_feature(atom: Atom, feature: str):
+    atom.feature &= ~(1 << HASH_FEATURE_TABLE[feature])
 
 
-def update_feature(element: Element, target_element: Element, connect_num: int, is_first=True):
-    if is_first:
-        feature_name = element.name + "-" + str(connect_num) + target_element.name
-        add_feature(element, "direct_connection", feature_name)
-    is_occupy = is_first
-    for indirect_connected in target_element.bond_list:
-        if indirect_connected is None:
-            if is_occupy:
-                is_occupy = False
-                continue
+def update_feature(atom: Atom, target_atom: Atom, connect_num: int):
+    # if is_first:
+    feature_name = atom.name + "-" + str(connect_num) + target_atom.name
+    add_feature(atom, feature_name)
+    # is_occupy = is_first
+    # for indirect_connected in target_element.bond_list:
+    #     if indirect_connected is None:
+    #         if is_occupy:
+    #             is_occupy = False
+    #             continue
 
 
-def connect(target_element_list, is_cyclization=False):
-    for tmp in range(len(target_element_list) - 1):
-        target_element_list[tmp].add_bond(target_element_list[tmp + 1])
+def connect(target_atom_list, is_cyclization=False):
+    for tmp in range(len(target_atom_list) - 1):
+        target_atom_list[tmp].add_bond(target_atom_list[tmp + 1])
     if is_cyclization:
-        target_element_list[0].add_bond(target_element_list[-1])
+        target_atom_list[0].add_bond(target_atom_list[-1])
 
 
-c1 = Element("C")
-c2 = Element("c")
-c3 = Element("c")
-c4 = Element("c")
-c5 = Element("c")
-c6 = Element("c")
-c7 = Element("c")
-# c1-c7 o1 构成苯醛
-o1 = Element("o")
-o2 = Element("o")
-o3 = Element("o")
-print(inquire_id(c1.feature))
-print(inquire_id(c2.feature))
-print(inquire_id(c3.feature))
-print(inquire_id(c4.feature))
+if __name__ == '__main__':
+    benzaldehyde = Molecule()
+    benzaldehyde.add_atom('c', 7)
+    benzaldehyde.add_atom('o')
 
-c1.add_bond(o1, 2)
-c1.add_bond(c2)
-connect([c2, c3, c4, c5, c6, c7], True)
+    c1 = Atom("C")
+    c2 = Atom("c")
+    c3 = Atom("c")
+    c4 = Atom("c")
+    c5 = Atom("c")
+    c6 = Atom("c")
+    c7 = Atom("c")
+    # c1-c7 o1 构成苯醛
+    o1 = Atom("o")
+    o2 = Atom("o")
+    o3 = Atom("o")
 
-c2.add_pi_pond([c2, c3, c4, c5, c6, c7])
-print(c1.name, id(c1))
-print()
-# for i in c1.bond_list:
-#     if type(i) is Element:
-#         print(i.name, i.feature, id(i))
-# print()
-# for i in c2.bond_list:
-#     if type(i) is Element:
-#         print(i.name, i.feature, id(i))
-#     elif type(i) is list:
-#         print()
-#         for j in i:
-#             print(j.name, j.feature, id(j))
-print(c1.bond_list, c1)
-print(c2.bond_list, c2.name)
-# print(element_id)
+    c1.add_bond(o1, 2)
+    c1.add_bond(c2)
+    connect([c2, c3, c4, c5, c6, c7], True)
+
+    c2.add_pi_pond([c2, c3, c4, c5, c6, c7])
+    print(c1.name, bin(c1.feature))
+    print(c1.feature)
+    # for i in c1.bond_list:
+    #     if type(i) is Element:
+    #         print(i.name, i.feature, id(i))
+    # print()
+    # for i in c2.bond_list:
+    #     if type(i) is Element:
+    #         print(i.name, i.feature, id(i))
+    #     elif type(i) is list:
+    #         print()
+    #         for j in i:
+    #             print(j.name, j.feature, id(j))
+    print(c1.bond_list, c1)
+    print(c2.bond_list, c2.name)
+    # print(element_id)
